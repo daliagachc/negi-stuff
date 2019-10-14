18b556f04550d8de484d86b15358368b405c5093
diego <diego.aliaga@helsinki.fi>
2019-10-14 23:26:53 +0300

    from_mac

---
+++ negi_stuff/presentations/intro_jlab/04-interaction.py
@@ -0,0 +1,57 @@
+# ---
+# jupyter:
+#   jupytext:
+#     text_representation:
+#       extension: .py
+#       format_name: percent
+#       format_version: '1.2'
+#       jupytext_version: 1.2.3
+#   kernelspec:
+#     display_name: Python 3
+#     language: python
+#     name: python3
+# ---
+
+# %%
+??negi_stuff.modules.imps
+
+# %% [markdown]
+#
+
+# %% [markdown]
+#
+
+# %% [markdown]
+# ## Functions
+# - Exploratory Data Analysis in Jupyter Lab
+#     - Positive
+#         - Fast
+#         - Prototype
+#         - Instant Feedback
+#     - Negative
+#         - Notebooks can become unmanagable
+#         - Tendency to do repetitive tasks
+
+# %% [markdown]
+# ## Imports
+
+# %%
+link to [imps](../../modules/imps.py) 
+
+# %% [markdown]
+# ## Scripts
+
+# %%
+link to [script](04-interaction.py)
+
+# %% [markdown]
+# ## Packages
+
+# %% [markdown]
+#
+
+# %% [markdown]
+#
+
+# %% [markdown]
+#
