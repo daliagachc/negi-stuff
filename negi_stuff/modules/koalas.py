969c1c58c7f2ef2b76a3566f45f57ecac1c3b424
diego <diego.aliaga@helsinki.fi>
2019-10-21 21:35:23 +0200

    from_mac

--- negi_stuff/modules/koalas.py
+++ negi_stuff/modules/koalas.py
@@ -13,7 +13,8 @@ import pandas as pd
 
 def check_transform_cftime_dim_2_timestamp(
         df:pd.DataFrame,
-        # time_name = 'time'
+        # time_name = 'time',
+        middle_of_month = False
 ) -> pd.DataFrame:
     '''
     fixes problems with cftime objects not being converted into timestamp
@@ -25,6 +26,10 @@ def check_transform_cftime_dim_2_timestamp(
     ----------
     df
         input
+    middle_of_month
+        if cftime.Datetime360Day then values like (2019,2,30) are allowed
+        but that makes no sense in pd.Timestamp so, we subtract 15 days to
+        be in the middle and avoid this
 
     Returns
     -------
@@ -52,6 +57,18 @@ def check_transform_cftime_dim_2_timestamp(
 
         time_xr_index = df.to_xarray()[orig_name]
         time_dim_string = time_xr_index.dt.strftime(time_first_element.format)
-        df.index = pd.to_datetime(time_dim_string)
+    #     df.index = pd.to_datetime(time_dim_string)
+    y = 'Year'
+    m = 'Month'
+    d = 'Day'
+    tc = [m,d,y]
+    time_xr_index[y] = time_xr_index.dt.year
+    time_xr_index[m] = time_xr_index.dt.month
+    time_xr_index[d] = time_xr_index.dt.day
+    if middle_of_month:
+        time_xr_index[d] = 15
+    _df = time_xr_index.to_dataframe().drop('time',axis=1).reset_index()
+    _df['time'] = pd.to_datetime(_df[tc])
+    df.index = _df['time']
 
     return df
