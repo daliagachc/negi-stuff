2690b20a72e2185b149beb8406b10af3bca54254
diego <diego.aliaga@helsinki.fi>
2019-10-27 02:22:33 +0300

    from_mac

--- negi_stuff/modules/ebas_db_get_info.py
+++ negi_stuff/modules/ebas_db_get_info.py
@@ -4,16 +4,21 @@ pya.const.BASEDIR = '/home/notebook/shared-ns1000k/inputs/pyaerocom-testdata/'
 def get_station_coords(station_name, unique=True):
     """
     If match unique, returns tuplet with (<station name>, <lon>, <lat>)
-    else prints message and returns Null. 
+    else prints message and returns Null.
     Example: Get station coords:
     lon, lat = get_station_coords('Zeppelin')[1:]
     """
     sql_db = pya.io.EbasFileIndex()
-    st_coor = sql_db.execute_request('select distinct station_name,station_longitude,station_latitude from station')
+    st_coor = sql_db.execute_request(
+        '''
+        select distinct 
+            station_name,station_longitude,station_latitude 
+        from station
+        ''')
     #station_name='Zeppelin'
     match_c = 0
     st_match=[]
-    
+
     for comb in st_coor:
         if unique: test = station_name.strip()==comb[0].strip()
         else: test= station_name in comb[0]
@@ -28,4 +33,4 @@ def get_station_coords(station_name, unique=True):
     else:
         print('Found more than one match, please specify:')
         print(st_match)
-    
+
